348a5d1e22f1dfe4ed894fa2d00309c2f6a912f9
chanken-tec <kentaro1016kh@icloud.com>
2021-02-21 16:07:57 +0900

    モデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, presence: true, format: {with: /\A[a-zA-Z0-9]+\z/}
+  validates :password_confirmation, presence: true, format: {with: /\A[a-zA-Z0-9]+\z/}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_ruby, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_ruby, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items
+  resources :users
+  resources :purchases
+
 
   root to: 'items#index'
 
