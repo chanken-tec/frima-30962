fee53c47739a9e34564962e543d33bc78e5a4102
chanken-tec <kentaro1016kh@icloud.com>
2021-03-02 22:39:06 +0900

    (修正2)モデルの単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :item do
     name             {Faker::Commerce.product_name}
     information      {Faker::Lorem.sentence}
-    category_id      {Faker::Number.between(from: 1, to: 11)}
-    condition_id     {Faker::Number.between(from: 1, to: 7)}
-    delivery_fee_id  {Faker::Number.between(from: 1, to: 3)}
-    shipping_area_id {Faker::Number.between(from: 1, to: 48)}
-    shipping_day_id  {Faker::Number.between(from: 1, to: 4)}
+    category_id      {Faker::Number.between(from: 2, to: 11)}
+    condition_id     {Faker::Number.between(from: 2, to: 7)}
+    delivery_fee_id  {Faker::Number.between(from: 2, to: 3)}
+    shipping_area_id {Faker::Number.between(from: 2, to: 48)}
+    shipping_day_id  {Faker::Number.between(from: 2, to: 4)}
     price            {1000} 
     association :user
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -38,10 +38,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
       end
 
+      it 'カテゴリー情報が1の場合登録できないこと' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+
       it '商品についての情報が必須であること' do
         @item.condition_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank", "Condition is not a number")
+      end
+
+      it '商品情報が1の場合登録できないこと' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
       end
 
       it '配送料の負担についての情報が必須であること' do
@@ -50,16 +62,34 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Delivery fee can't be blank", "Delivery fee is not a number")
       end
 
+      it '配送料情報が1の場合登録できないこと' do
+        @item.delivery_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee must be other than 1")
+      end
+
       it '発送元の地域についての情報が必須であること' do
         @item.shipping_area_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping area can't be blank", "Shipping area is not a number")
       end
 
+      it '発送元情報が1の場合登録できないこと' do
+        @item.shipping_area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area must be other than 1")
+      end
+
       it '発送までの日数についての情報が必須であること' do
         @item.shipping_day_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping day can't be blank", "Shipping day is not a number")
+      end
+
+      it '発送までの情報が1の場合登録できないこと' do
+        @item.shipping_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day must be other than 1")
       end
 
       it '価格についての情報が必須であること' do
@@ -74,11 +104,30 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price is out of setting range")
       end
 
+      it '価格の範囲が、¥300~¥9,999,999の間であること' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+
       it '販売価格は半角数字のみ保存可能であること' do
         @item.price = '１０００'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number", "Price is out of setting range")
       end
+
+      it '販売価格は半角英数混合では登録できないこと' do
+        @item.price = '100a'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number", "Price is out of setting range")
+      end
+
+      it '販売価格は半角英語では登録できないこと' do
+        @item.price = 'aaa'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number", "Price is out of setting range")
+      end
+
     end
   end
 end
