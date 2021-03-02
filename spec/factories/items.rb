5cac9f952d9ac923bf4b3ca5ec1eece354be2c63
chanken-tec <kentaro1016kh@icloud.com>
2021-03-02 21:23:31 +0900

    (修正1)モデルの単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,13 @@ FactoryBot.define do
   factory :item do
     name             {Faker::Commerce.product_name}
     information      {Faker::Lorem.sentence}
-    category_id      {Faker::Number.between(1,11)}
-    condition_id     {Faker::Number.between(1,7)}
-    delivery_fee_id  {Faker::Number.between(1,3)}
-    shipping_area_id {Faker::Number.between(1,48)}
-    shipping_day_id  {Faker::Number.between(1,4)}
+    category_id      {Faker::Number.between(from: 1, to: 11)}
+    condition_id     {Faker::Number.between(from: 1, to: 7)}
+    delivery_fee_id  {Faker::Number.between(from: 1, to: 3)}
+    shipping_area_id {Faker::Number.between(from: 1, to: 48)}
+    shipping_day_id  {Faker::Number.between(from: 1, to: 4)}
     price            {1000} 
     association :user
   end
 end
+
