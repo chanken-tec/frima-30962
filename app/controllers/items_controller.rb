394f04ffb2272e06931ab6df6001496b92f82fda
chanken-tec <kentaro1016kh@icloud.com>
2021-03-12 17:31:57 +0900

    提出前修正4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,7 +52,6 @@ class ItemsController < ApplicationController
 
   def item_user
     if @item.user_id == current_user.id && @item.order.present?
-    else
       redirect_to root_path
     end
   end
